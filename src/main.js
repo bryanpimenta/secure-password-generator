4b4ed21688fb68460259b3af88eaa485f582e185
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-16 20:23:56 -0300

    Adicionada a função copy com o CLipboard copy

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { nanoid } from 'nanoid';
 import './style.css';
 
+import copy from 'clipboard-copy';
+
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
 
 passwordBtnEl.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPasswordEl.innerHTML = randomPassword;
+    copy(displayPasswordEl.innerHTML);
 });
